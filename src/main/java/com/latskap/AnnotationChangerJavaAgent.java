b2cea6e3bd2cb98cc653fb85526dfe8f373a4557
Raya A <r.abuzyarova@gmail.com>
2021-07-21 23:36:51 +0300

    ignore java agent exceptions

--- src/main/java/com/latskap/AnnotationChangerJavaAgent.java
+++ src/main/java/com/latskap/AnnotationChangerJavaAgent.java
@@ -1,46 +1,31 @@
 package com.latskap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
-import java.util.Optional;
 
 public class AnnotationChangerJavaAgent {
-    private static final Logger log = LoggerFactory.getLogger(AnnotationChangerJavaAgent.class);
 
     public static void premain(String args, Instrumentation instrumentation) {
-        ParsedArgs parsedArgs = new ParsedArgs(args);
         try {
-            transform(parsedArgs, instrumentation);
-        } catch (Exception e) {
-            log.error("Not possible to change current run configuration." + e.getMessage());
+            transform(new ParsedArgs(args), instrumentation);
+        } catch (Exception ignored) {
         }
     }
 
-    private static void transform(ParsedArgs parsedArgs, Instrumentation instrumentation) {
-        getClassByName(parsedArgs.getTargetClass(), instrumentation)
-                .ifPresent(targetClass -> {
-                            instrumentation.addTransformer(new MyClassTransformer(targetClass, parsedArgs), true);
-                            try {
-                                instrumentation.retransformClasses(targetClass);
-                            } catch (UnmodifiableClassException e) {
-                                log.error("Not possible to change current run configuration. " +
-                                        "Can not transform class: [" + targetClass.getName() + "]." + e.getMessage());
-                            }
-                        }
-                );
+    private static void transform(ParsedArgs parsedArgs, Instrumentation instrumentation) throws UnmodifiableClassException {
+        Class<?> targetClass = getClassByName(parsedArgs.getTargetClass(), instrumentation);
+        instrumentation.addTransformer(new MyClassTransformer(targetClass, parsedArgs), true);
+        instrumentation.retransformClasses(targetClass);
     }
 
-    private static Optional<Class<?>> getClassByName(String className, Instrumentation instrumentation) {
+    private static Class<?> getClassByName(String className, Instrumentation instrumentation) {
         try {
-            return Optional.of(Class.forName(className));
+            return Class.forName(className);
         } catch (ClassNotFoundException e) {
-            for (Class<?> clazz : instrumentation.getAllLoadedClasses())
-                if (clazz.getName().equals(className))
-                    return Optional.of(clazz);
+            for (Class<?> cl : instrumentation.getAllLoadedClasses())
+                if (cl.getName().equals(className))
+                    return cl;
         }
-        return Optional.empty();
+        throw new RuntimeException("Failed to find class [" + className + "].");
     }
 }
